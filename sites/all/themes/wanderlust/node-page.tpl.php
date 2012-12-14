33ab2f80c87e23edaeccd029541602e40c794de0
root <root@wanderlust.(none)>
2012-12-14 08:35:31 -0600

    views tabs

--- sites/all/themes/wanderlust/node-page.tpl.php
+++ sites/all/themes/wanderlust/node-page.tpl.php
@@ -1,16 +1,16 @@
 <?php
-  $path = explode('sites:3', $node->path);
-  $path = isset($path[1]) ? $path[1] : '';
-  $_page_url = $_SERVER['SERVER_NAME'].'/'.arg(0).'/'.arg(1);
+  //$path = explode('sites:3', $node->path);
+  //$path = isset($path[1]) ? $path[1] : '';
+  //$_page_url = $_SERVER['SERVER_NAME'].'/'.arg(0).'/'.arg(1);
   
 ?>
 <div id="node-<?php print $node->nid; ?>" class="node-<?php print $node->type; ?> node<?php if ($sticky) { print ' sticky'; } ?><?php if (!$status) { print ' node-unpublished'; } ?> clearfix">
   <div class="page-description">
     <?php print $node->field_page_description[0]['view']; ?>
   </div><!-- page description -->
-  <div class="social-networks">
+  <!--<div class="social-networks">
     <div class="like-facebook">
-      <iframe src="//www.facebook.com/plugins/like.php?href=http://<?php print $_page_url; ?>;send=false&amp;layout=button_count&amp;width=450&amp;show_faces=true&amp;action=like&amp;colorscheme=light&amp;font&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:21px;" allowTransparency="true"></iframe>
+      <iframe src="//www.facebook.com/plugins/like.php?href=http://<?php// print $_page_url; ?>;send=false&amp;layout=button_count&amp;width=450&amp;show_faces=true&amp;action=like&amp;colorscheme=light&amp;font&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:21px;" allowTransparency="true"></iframe>
     </div>
     <div class="like-twitter">
       <script src="//platform.twitter.com/widgets.js" type="text/javascript"></script>
