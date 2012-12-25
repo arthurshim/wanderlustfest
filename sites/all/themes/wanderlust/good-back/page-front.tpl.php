be2346e179f28c65545593e662a94b8c9612b87b
freeenergy <rahul@nohup.in>
2012-12-25 17:07:43 +0000

    mobile if statement for jquery and hovers

--- sites/all/themes/wanderlust/good-back/page-front.tpl.php
+++ sites/all/themes/wanderlust/good-back/page-front.tpl.php
@@ -542,18 +542,15 @@ function doSubmit(oForm) {
 
 </body>
 <script>
+if ( navigator.userAgent.indexOf('iPhone') == -1 || navigator.userAgent.indexOf('Android') == -1  )
+  {
 
-
+$(function() {
+        $( "#legend" ).draggable();
+		   $( "#joinmail" ).draggable();
+    });
 	
-	
-
-
-
-
-
-
-
-  
+  }
 /*detect if mobile */
 var isMobile = {
     Android: function() {
@@ -578,105 +575,12 @@ var isMobile = {
 
 
 if( isMobile.any() ) {
-
-var script = document.createElement( 'script' );
-script.value = 'type="text/javascript" src="/sites/all/modules/wl_helper/wl_helper_mobile.js"';
-$("head").prepend( script );
-
- 
+ $('head').append('<script type="text/javascript" src="/sites/all/modules/wl_helper/wl_helper_mobile.js"></script>');
 
 } 
 
-
-
 else {
-
-  $(document).ready(function () {
-
-        $( "#legend" ).draggable();
-		   $( "#joinmail" ).draggable();
-
-
-
-if (!$(this).hasClass("test")) {
- 
-$(".mark:not(.legendNo)").hover(function(){
-
-   $(this).find('div.innersite').fadeIn();
-   if ($(this).hasClass("festival")) {
-     /*$('div.mark.festival.legendNo').addClass('highlight');  */ 
-   }
-   else if($(this).hasClass('yoga')) {
-     /* $('div.mark.yoga.legendNo').addClass('highlight');*/   
-   }
-   else if($(this).hasClass('studio')){
-     /* $('#div.mark.studio.legendNo').addClass('highlight'); */  
-   }
-   
-},function(){
-   $(this).find('div.innersite').hide();
-   $('div.mark.festival.legendNo, div.mark.studio.legendNo, div.mark.yoga.legendNo').removeClass('highlight');     
-});
-}
-
-
-/*hover legend*/
-   $("#legend1 .mark").hover(function() {
-   $(".mark.festival").addClass('highlight1');
-   },
-   function(){
-   $('#.mark.festival').removeClass('highlight1'); 
-   });
-   
-      
-   $("#legend2 .mark").hover(function() {
-   $(".mark.yoga").addClass('highlight1');
-   },
-   function(){
-   $('#.mark.yoga').removeClass('highlight1'); 
-   });
-   
-      
- /*for star only*/
-
-   $("div.mark.studio").hover(function() {
-   $(".mark.studio").addClass('highlight2');
-   },
-   function(){
-   $('#.mark.studio').removeClass('highlight2'); 
-   });
-
-  
-  $('body').append('<div id="ted_photo_hover"></div>');
-    $('#ted_photo_hover').css({position: 'absolute'});
-
-    $('#ted_photo_hover').offset({top: 500, left: 500});
-  $('.ted_photo').hover(
-  function(e){
-    var name      = $(this).children('.ted_photo_data').html();
-    var answer    = $(this).children('a').children('img').attr('title');
-    var eventName = $(this).children('a').children('img').attr('alt');
-
-    var hoverText = "";
-    hoverText    += "Name: " + name + '<br>\r\n'; 
-    hoverText    += "Event: " + eventName + '<br>\r\n'; 
-    hoverText    += '"' + answer + '"'; 
-  //    $('#ted_photo_hover').show();
-    $('#ted_photo_hover').html(hoverText);
-    $('#ted_photo_hover').css({top: e.pageY, left: e.pageX});
-      divoff = $('#ted_photo_hover').offset();
-  //    alert(e.pageX +' '+ e.pageY);
-  //    $(this).children('a').children('img').attr('title', hoverText);
-  //    $(this).children('img').attr('title', hoverText);
-    //        $('#ted_photo_hover').css({'left': e.pageX + 'px', 'bottom': e.pageY + 'px', 'position': 'absolute'});
-  },
-  function(e){
-  //    $('#ted_photo_hover').hide();
-  });
-  
-  
-  
-});
+ $('head').append('<script type="text/javascript" src="/sites/all/modules/wl_helper/wl_helper.js"></script>');
 
 }
 </script>
