b969991ab82f23ddb6855942d105543472f6a322
freeenergy <asborsody@gmail.com>
2013-01-15 20:37:51 +0000

    updated page wide -arthur

--- sites/all/themes/wanderlust/page-wide.tpl.php
+++ sites/all/themes/wanderlust/page-wide.tpl.php
@@ -162,23 +162,7 @@ _gaq.push(['_trackPageview']);
       </div> <!-- /#main -->
       
     </div>
-      
-    <footer id="footer" role="contentinfo" class="clearfix">
-      <?php if (!empty($footertop)): ?>
-      <div class="footertop">
-        <div id="footer-top-inner">
-        <?php print $footertop; ?>
-        </div>
-      </div>
-      <?php endif; ?>
-      <div class="subhead">
-        <?php if (!empty($footer)): print $footer; endif; ?>
-        <?php print $feed_icons ?>
-      </div>
-      <div class="footer-message">
-        <div class="footer-message-content"><?php print $footer_message; ?></p>
-      </div>
-    </footer> <!-- /#footer -->
+    
 
     <?php print $closure ?>
 
