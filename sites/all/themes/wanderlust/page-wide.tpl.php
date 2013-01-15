4eae50163bbc402aebdf0192b7e48b99279b8602
freeenergy <asborsody@gmail.com>
2013-01-15 20:36:35 +0000

    modified page-wide for schedule pages -arthur

--- sites/all/themes/wanderlust/page-wide.tpl.php
+++ sites/all/themes/wanderlust/page-wide.tpl.php
@@ -1,32 +1,33 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]> <html class="ie6 ie" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"> <![endif]-->
-<!--[if IE 7]>    <html class="ie7 ie" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"> <![endif]-->
-<!--[if IE 8]>    <html class="ie8 ie" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"> <![endif]-->
-<!--[if gt IE 8]> <!--> <html class="" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"> <!--<![endif]-->
+<!--page.tpl.php -->
+<!--[if lt IE 7]> <html xmlns:og="http://opengraphprotocol.org/schema/" class="ie6 ie" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"> <![endif]-->
+<!--[if IE 7]>    <html xmlns:og="http://opengraphprotocol.org/schema/" class="ie7 ie" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"> <![endif]-->
+<!--[if IE 8]>    <html xmlns:og="http://opengraphprotocol.org/schema/" class="ie8 ie" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"> <![endif]-->
+<!--[if gt IE 8]> <!--> <html xmlns:og="http://opengraphprotocol.org/schema/" class="" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"> <!--<![endif]-->
 <head>
   <?php print $head; ?>
   <!-- Set the viewport width to device width for mobile -->
   <meta name="viewport" content="width=device-width" />
   <title><?php print $head_title; ?></title>
+  <?php header("Connection: keep-alive");?>
   <?php print $styles; ?>
   <?php print $scripts; ?>
-  <!-- IE Fix for HTML5 Tags -->
+  
+    <!-- IE Fix for HTML5 Tags -->
   <!--[if lt IE 9]>
     <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
   <![endif]-->
   
-   <meta property="og:description" content="Wanderlust is a one-of-a-kind festival bringing together the world's leading yoga teachers, top musical acts and DJs, renowned speakers, top chefs and winemakers, and much, much more -- all in a setting of breathtaking natural beauty."/>
-   <meta property="og:title" content="Wanderlust Festival 2012"/>
-   <meta property="og:type" content="website"/>
-   <!--<meta property="og:url" content="http://www.wanderlustfestival.com"/>-->
-   <meta property="og:image" content="http://wanderlustfestival.com/wlbird.jpg"/>
-   <meta property="og:site_name" content="Wanderlust"/>
-   <meta property="fb:admins" content="1407657,1149492126,515565663,508316132,6911729"/>
-<meta property="fb:app_id" content="321576251186267"/>
+ 
+  <!-- SET FACEBOOK OPEN GRAPH APP ID -->
+  <meta property="fb:app_id" content="321576251186267"/>
 
-    <script type="text/javascript">
+ 
+
+  <!-- <script type="text/javascript">
     // cross domain tracking code for Schedule Activities link - to open in new window
-    function openWindow(link) {
+  
+	function openWindow(link) {
       _gaq.push(function() {
         var tracker = _gaq._getAsyncTracker();  //add name param if needed
         window.open(tracker._getLinkerUrl(link.href));
@@ -36,13 +37,66 @@
 
 
   </script>
+
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-12068308-1']);
+  _gaq.push(['_setDomainName', 'wanderlustfestival.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+  <script type="text/javascript">
+function recordOutboundLink(link, category, action) {
+try {
+var myTracker=_gat._getTrackerByName();
+_gaq.push(['myTracker._trackEvent', category , action ]);
+setTimeout('document.location = "' + link.href + '"', 100)
+}catch(err){}
+}
+</script>
+-->
+
+<script>
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-12068308-1']);
+_gaq.push(['_setAllowLinker', true]);
+_gaq.push(['_setDomainName', 'wanderlustfestival.com']);
+_gaq.push(['_trackPageview']);
+
+
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+  	
     <?php
       $base = wl_get_base_domain();
       ?>
+
+
+<!-- SHARE THIS JAVASCRIPT -->
+<script type="text/javascript">var switchTo5x=true;</script>
+<script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
+<script type="text/javascript">stLight.options({publisher: "8257ea56-556c-4bcc-90d4-07b6f18c3693",  shorten:false});</script>
+
+
   
 </head>
 
-<body class="<?php print $body_classes; ?> wide-template">
+<body class="<?php print $body_classes; ?>">
   <?php print $google_marketing; ?>
   <div id="fb-root"></div>
   <script>
@@ -64,51 +118,34 @@
       <?php endif; ?>
     </div>
 
+ 
+    <!-- BEGIN PAGE.TPL.PHP HEADER -->
     <header id="header" role="banner" class="clearfix">
-      
-   
-      
-   <div class="subhead">
-           <hgroup id="site-name-slogan">      
-           <?php print $header; ?>
-           </hgroup>
 
-    <div class="newlogo">
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" id="logo"><img src="<?php print  base_path() . path_to_theme() . '/images/new_logo.png'//print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
-   <?php print $topnav; ?>
-    </div>
-
-   <div class="tomap">
-      <a href="<?php print "http://{$base}"; ?>" title="<?php print t('To map'); ?>"><img src="<?php print  base_path() . path_to_theme() . '/images/newmap_icon.png'//print $logo; ?>" alt="<?php print t('To map'); ?>" /></a>
-   </div>
-
-  </div>
-      
-      <?php if ($search_box): ?><?php print $search_box ?><?php endif; ?>
-      
-	    <?php if ($primary_links || $secondary_links || !empty($navigation)): ?>
-      <nav id="navigation" role="navigation" class="clearfix ">
-	      <div class="subhead2"><?php print $navigation ?></div>
-      </nav> <!-- /#navigation -->
-      <?php endif; ?>
-      
-    </header> <!-- /#header -->
-   
-    <div id="slideshows" role="slideshow" class="slideshowhome">
-      <?php if (!empty($slidemain)): ?>
-      <div class="slidemain-container">
-        <div class="slidemain">
-          <?php print $slidemain; ?>
-        </div>
+      <div id="event-info">      
+        <?php print $header; ?>
       </div>
-      <?php endif; ?>
-       
-	    <?php if (!empty($slides)): ?>
-      <div class="slides">
-        <?php print $slides; ?>
+
+      <div class="tomap">
+        <a href="<?php print "http://{$base}"; ?>" title="<?php print t('To map'); ?>"><img src="http://9394bc4f934eb8c957d8-2f084e1f525b6270d41d6d2c79f4c609.r93.cf1.rackcdn.com/2013/global-splash-callout.jpg" alt="<?php print t('To map'); ?>" /></a>
       </div>
-      <?php endif; ?>
-    </div>
+      
+    </header>
+    <!-- END HEADER -->
+    
+    <!-- BEGIN NAVIGATION -->  
+    <nav id="navigation" role="navigation" class="clearfix ">
+      <?php print $navigation ?>
+      <div class="social-media-links">
+        <a class="facebook" href="http://facebook.com/wanderlust" target="_blank">Facebook</a>
+        <a class="twitter" href="http://twitter.com/wanderlustfest" target="_blank">Twitter</a>
+        <a class="instagram" href="http://instagram.com/wanderlustfest" target="_blank">Instagram</a>
+        <a class="youtube" href="http://youtube.com/wanderlustfestival" target="_blank">YouTube</a>
+        <a class="email" >Email</a>        
+      </div>      
+    </nav> 
+    <!-- END NAVIGATION -->
+
     
     <div class="subhead body-content">
     
@@ -146,6 +183,26 @@
     <?php print $closure ?>
 
   </div> <!-- /#container -->
+
+
+
+ <!-- BEGIN FOOTER -->
+  <footer class="footer">
+    <div class="logos">
+      <div class="velour">Velour Music Group</div>
+      <div class="c3">C3 Presents</div>
+    </div>
+    <div class="copy">&copy; <?php echo date("Y"); ?> Wanderlust Festival LLC.</div>
+    <div class="footer-links">
+      <a href="<?php global $base_url; print $base_url; ?>/privacy-policy">Privacy Policy</a> |
+      <a href="<?php print $base_url; ?>/terms-of-use">Terms of Use</a>
+    </div>
+    <div class="site-credits">
+      Site by <a href="http://rootdownmedia.com" target="_blank">Root Down</a>
+    </div>
+  </footer>
+  <!-- END FOOTER -->
+
 
 </body>
 </html>
