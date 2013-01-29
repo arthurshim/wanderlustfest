1d528103146645ce1fcdff1bfa428b088782de5a
freeenergy <asborsody@gmail.com>
2013-01-29 20:30:01 +0000

    new tweaks arthur

--- sites/all/themes/wanderlust/page-media-photos.tpl.php
+++ sites/all/themes/wanderlust/page-media-photos.tpl.php
@@ -24,7 +24,7 @@
       
 
 	<script type="text/javascript" src="/sites/all/themes/wanderlust/js/fancybox/source/helpers/jquery.fancybox-thumbs.js"></script>
-	<script type="text/javascript" src="/sites/all/themes/wanderlust/js/fancybox/source/helpers/jquery.fancybox-media.js"></script>zz
+	<script type="text/javascript" src="/sites/all/themes/wanderlust/js/fancybox/source/helpers/jquery.fancybox-media.js"></script>
   <!-- IE Fix for HTML5 Tags -->
   <!--[if lt IE 9]>
     <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
