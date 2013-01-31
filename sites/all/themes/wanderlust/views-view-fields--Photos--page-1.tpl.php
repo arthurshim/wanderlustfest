482c88f6c38f8aeffb9823fced2781e072fe51d0
freeenergy <asborsody@gmail.com>
2013-01-31 16:32:16 +0000

    rm social buttons from photos and vdieos arthur

--- sites/all/themes/wanderlust/views-view-fields--Photos--page-1.tpl.php
+++ sites/all/themes/wanderlust/views-view-fields--Photos--page-1.tpl.php
@@ -1,10 +1,20 @@
+
+
 <?php $_page_url = $_SERVER['SERVER_NAME'].'/'.arg(0).'/'.arg(1);
+
+
+
 
 
 
 //print $fields['field_photos_images_fid']->content;
 print $fields['filepath']->content; ?>
 <div class="views-field-title"><?php print $fields['title']->content; ?></div>
+
+
+
+<?php /* COMMENTED OUT SOCIAL BUTTONS
+
 <div class="social-networks">
   <div class="like-twitter">
     <script src="//platform.twitter.com/widgets.js" type="text/javascript"></script>
@@ -14,3 +24,5 @@ print $fields['filepath']->content; ?>
     <iframe src="//www.facebook.com/plugins/like.php?href=<?php print $_page_url ?>;send=false&amp;layout=button_count&amp;width=450&amp;show_faces=true&amp;action=like&amp;colorscheme=light&amp;font&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:21px;" allowTransparency="true"></iframe>
   </div>
 </div><!-- social networks -->
+
+*/ ?>
