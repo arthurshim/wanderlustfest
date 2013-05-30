2b1282972e7443eddc7309e93f3f7baf98ac0971
freeenergy <asborsody@gmail.com>
2013-05-30 16:44:40 +0000

    analytics google

--- sites/all/themes/wanderlust/page-media-photos.tpl.php
+++ sites/all/themes/wanderlust/page-media-photos.tpl.php
@@ -45,20 +45,6 @@
   });
 })(jQuery);
 });
-
-  var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-12068308-1']);
-_gaq.push(['_setAllowLinker', true]);
-_gaq.push(['_setDomainName', 'wanderlustfestival.com']);
-_gaq.push(['_trackPageview']);
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-
-
 </script>
 
 
@@ -233,6 +219,19 @@ var google_conversion_label = "NKVFCIGI3AQQzYO75gM";
 var google_custom_params = window.google_tag_params;
 var google_remarketing_only = true;
 /* ]]> */
+</script>
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-12068308-1']);
+  _gaq.push(['_setDomainName', 'wanderlustfestival.com']);
+  _gaq.push(['_setAllowLinker', true]);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
 </script>
 </body>
 </html>
--- sites/all/themes/wanderlust/page-search-node.tpl.php
+++ sites/all/themes/wanderlust/page-search-node.tpl.php
@@ -42,15 +42,16 @@ jQuery.noConflict();
 
 
 
-<script>
+<script type="text/javascript">
   var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-12068308-1']);
-_gaq.push(['_setAllowLinker', true]);
-_gaq.push(['_setDomainName', 'wanderlustfestival.com']);
-_gaq.push(['_trackPageview']);
+  _gaq.push(['_setAccount', 'UA-12068308-1']);
+  _gaq.push(['_setDomainName', 'wanderlustfestival.com']);
+  _gaq.push(['_setAllowLinker', true]);
+  _gaq.push(['_trackPageview']);
+
   (function() {
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
 </script>
