617d7e3499351a92f7710b626d5ee465dd71052a
freeenergy <asborsody@gmail.com>
2013-10-07 18:01:12 +0000

    new adroll tracking code to page tpls

--- sites/all/themes/wanderlust/page.tpl.php
+++ sites/all/themes/wanderlust/page.tpl.php
@@ -292,5 +292,25 @@ var google_remarketing_only = true;
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
 </script>
+
+<!-- AdRoll code -->
+<script type="text/javascript">
+adroll_adv_id = "RZRFLXHNUZGIDJYVUM2UXM";
+adroll_pix_id = "2525WMUBZVGTZFRBIAHWGZ";
+(function () {
+var oldonload = window.onload;
+window.onload = function(){
+   __adroll_loaded=true;
+   var scr = document.createElement("script");
+   var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");
+   scr.setAttribute('async', 'true');
+   scr.type = "text/javascript";
+   scr.src = host + "/j/roundtrip.js";
+   ((document.getElementsByTagName('head') || [null])[0] ||
+    document.getElementsByTagName('script')[0].parentNode).appendChild(scr);
+   if(oldonload){oldonload()}};
+}());
+</script>
+
 </body>
 </html>
