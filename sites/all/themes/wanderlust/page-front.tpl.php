d782e8cfdf672481395e3125a3f798bcfef0a218
freeenergy <asborsody@gmail.com>
2014-01-06 21:43:50 +0000

    new global topbar html and css arthur (do not push live until feb 10)

--- sites/all/themes/wanderlust/page-front.tpl.php
+++ sites/all/themes/wanderlust/page-front.tpl.php
@@ -343,6 +343,173 @@ function doSubmit(oForm) {
     }(document, 'script', 'facebook-jssdk'));
   </script>
   
+  
+  <style type="text/css">
+    #wl-global-bar{
+      background: #2b1d0b;
+      width: 100%;
+      height: 38px;
+      text-transform: uppercase;
+    }
+    
+    #wl-global-bar a{
+      color: #b7b4b2;
+    }
+    
+    #wl-global-bar li{
+      display: inline;
+      list-style-type: none;
+    }
+    
+    #global-nav{
+      float: left;
+    }
+    
+    #global-logo{
+      float: left;
+      position: relative;
+      top: 11px;
+      left: 30px;
+    }
+    
+    #wl-global-social{
+      float: right;
+      position: relative;
+      top: 11px;
+      right: 10px;
+    }
+    
+    #global-nav{
+      position: relative;
+      top: 14px;
+      left: 50px;
+    }
+    
+    #global-nav li{
+      margin-right: 30px;
+      font-size: 14px;
+    }
+    
+    #global-logo a{
+      display: block;
+      text-indent: -999em;
+      background: url('http://9394bc4f934eb8c957d8-2f084e1f525b6270d41d6d2c79f4c609.r93.cf1.rackcdn.com/2014-images/wl-global-topbar-sprite.png');
+      width: 158px; 
+      height: 17px; 
+    }
+    
+    #find-your-true-north span{
+      width: 101px; 
+      height: 14px; 
+      text-indent: -999em;
+      background: url('http://9394bc4f934eb8c957d8-2f084e1f525b6270d41d6d2c79f4c609.r93.cf1.rackcdn.com/2014-images/wl-global-topbar-sprite.png');
+      background-position: 0px -21px; 
+      display: inline-block;   
+      margin-right: 40px;   
+      position: relative;
+      top: 2px;
+    }
+    
+    #wl-global-social li{
+      margin-right: 18px;
+    }
+    
+    #wl-global-social a{
+      background: url('http://9394bc4f934eb8c957d8-2f084e1f525b6270d41d6d2c79f4c609.r93.cf1.rackcdn.com/2014-images/wl-global-topbar-sprite.png');
+      display: inline-block;
+      text-indent: -999em;
+
+    }
+    #wl-global-social .facebook a{ 
+      background-position: 0px -40px; 
+      width: 8px; 
+      height: 15px;  
+    } 
+    
+    #wl-global-social .instagram a{ 
+      background-position: 0px -60px; 
+      width: 15px; 
+      height: 15px;  
+      } 
+
+    #wl-global-social .twitter a{ 
+    background-position: 0px -79px; 
+    width: 16px; 
+    height: 16px;  
+    } 
+    
+    #wl-global-social .youtube a { 
+    background-position: 0px -98px; 
+    width: 19px; 
+    height: 16px;  
+    }    
+    
+    #wl-global-social .youtube {
+      margin-right: 0 !important;
+    } 
+    
+    #global-nav a{
+      display: inline-block;
+      text-indent: -999em;
+      background: url('http://9394bc4f934eb8c957d8-2f084e1f525b6270d41d6d2c79f4c609.r93.cf1.rackcdn.com/2014-images/wl-global-topbar-sprite.png');
+
+    }
+    
+    #global-nav .festivals a{ 
+      background-position: 0px -119px; 
+      width: 70px; 
+      height: 11px;  
+      } 
+    
+    #global-nav .teacher-training a{ 
+      background-position: 0px -134px; 
+      width: 128px; 
+      height: 11px;  
+      } 
+    
+    #global-nav .studios a{ 
+      background-position: 0px -150px; 
+      width: 60px; 
+      height: 11px;  
+
+      } 
+    
+    #global-nav .media a{ 
+    background-position: 0px -165px; 
+    width: 42px; 
+      height: 11px;  
+    } 
+    
+    #global-nav .store a{ 
+    background-position: 0px -181px; 
+    width: 44px; 
+      height: 11px;  
+    } 
+    
+  </style>
+  
+  
+  <div id="wl-global-bar">
+    <div id="global-logo">
+      <a href="http://wanderlust.com">Wanderlust</a>
+    </div>
+    <ul id="global-nav">
+      <li class="festivals"><a href="http://wanderlust.com" target="_blank">Festivals</a></li>
+      <li class="teacher-training"><a href="http://wanderlust.com" target="_blank">Teacher Training</a></li>
+      <li class="studios"><a href="http://wanderlust.com" target="_blank">Studios</a></li>
+      <li class="media"><a href="http://wanderlust.com" target="_blank">Media</a></li>
+      <li class="store"><a href="http://wanderlust.com" target="_blank">Store</a></li>
+    </ul>
+    <ul id="wl-global-social">
+      <li id="find-your-true-north"><span>find your true north</span></li>
+      <li class="facebook"><a href="http://wanderlust.com" target="_blank">Facebook</a></li>
+      <li class="twitter"><a href="http://wanderlust.com" target="_blank">Twitter</a></li>
+      <li class="instagram"><a href="http://wanderlust.com" target="_blank">Instagram</a></li>
+      <li class="youtube"><a href="http://wanderlust.com" target="_blank">YouTube</a></li>
+    </ul>
+  </div>
+  
+  
   <div id="container" class="clearfix">
     <?php  if (!$f) { ?>
     <div id="skip-link">
