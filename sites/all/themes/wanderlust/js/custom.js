2a7c0901db3349c552f0d17d6dbe74efb7184de7
freeenergy <asborsody@gmail.com>
2014-01-22 20:23:16 +0000

    new ga tracking code

--- sites/all/themes/wanderlust/js/custom.js
+++ sites/all/themes/wanderlust/js/custom.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-
+/* OLD CODE
 // Google Analytics Cross Domain Tracking Code
 // Add onClick _link to all <a> elements on page where href contains frontgatetickets.com
 
@@ -10,6 +10,34 @@ $("a[href*='frontgatetickets.com']").click(function() {
   return false;
 
 });
+
+*/
+
+
+ // Google Analytics Cross Domain Tracking - Dynamically add GA code to outbound links
+
+  $("a[href*='wanderlust.com']").click(function () {
+    var pageTracker = _gat._getTrackerByName();
+    this.href = pageTracker._getLinkerUrl( this.href );
+  });
+
+  $("a[href*='welcu.com']").click(function () {
+    var pageTracker = _gat._getTrackerByName();
+    this.href = pageTracker._getLinkerUrl( this.href );
+  });
+
+  $("a[href*='frontgatetickets.com']").click(function () {
+    var pageTracker = _gat._getTrackerByName();
+    this.href = pageTracker._getLinkerUrl( this.href );
+  });
+
+
+  $("a[href*='sched.org']").click(function () {
+    var pageTracker = _gat._getTrackerByName();
+    this.href = pageTracker._getLinkerUrl( this.href );
+  });
+  
+
 
 
 });
