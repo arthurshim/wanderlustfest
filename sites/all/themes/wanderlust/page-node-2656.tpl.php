167904eb54c5543a7f8c8a46e3db57cb9e2fa06d
freeenergy <asborsody@gmail.com>
2014-01-28 21:18:36 +0000

    added freshdesk js to page tpls arthur

--- sites/all/themes/wanderlust/page-node-2656.tpl.php
+++ sites/all/themes/wanderlust/page-node-2656.tpl.php
@@ -275,6 +275,13 @@ window.onload = function(){
    if(oldonload){oldonload()}};
 }());
 </script>
+
+<!-- Freshdesk code -->
+<script type="text/javascript" src="http://assets.freshdesk.com/widget/freshwidget.js"></script>
+<script type="text/javascript">
+FreshWidget.init("", {"queryString": "&widgetType=popup&submitThanks=Thanks+for+your+feedback.++We'll+get+back+to+you+as+soon+as+possible.", "widgetType": "popup", "buttonType": "text", "buttonText": "Ask Us", "buttonColor": "white", "buttonBg": "#006063", "alignment": "4", "offset": "235px", "submitThanks": "Thanks for your feedback.  We'll get back to you as soon as possible.", "formHeight": "500px", "url": "http://support.wanderlustfestival.com"} );
+</script>
+
   
 </body>
 </html>
