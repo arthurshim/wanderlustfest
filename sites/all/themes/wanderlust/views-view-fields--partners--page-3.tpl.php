5a080e98ee2ac11158ed520347cf5ab8ec858333
freeenergy <asborsody@gmail.com>
2014-05-28 17:54:18 +0000

    alex's changes

---
+++ sites/all/themes/wanderlust/views-view-fields--partners--page-3.tpl.php
@@ -0,0 +1,27 @@
+<?php foreach ($fields as $id => $field): ?>
+  <?php
+  /*
+  global $user;
+  
+  if($user->uid == 1) {
+    continue;
+    drupal_set_message('<pre>' . print_r($field, 1) . '</pre>');
+  }*/
+  
+  if (!empty($field->separator)): ?>
+    <?php print $field->separator; ?>
+  <?php endif; ?>
+
+  <<?php print $field->inline_html;?> class="views-field-<?php print $field->class; ?>">
+    <?php if ($field->label): ?>
+      <label class="views-label-<?php print $field->class; ?>">
+        <?php print $field->label; ?>:
+      </label>
+    <?php endif; ?>
+      <?php
+      // $field->element_type is either SPAN or DIV depending upon whether or not
+      // the field is a 'block' element type or 'inline' element type.
+      ?>
+      <<?php print $field->element_type; ?> class="field-content"><?php print $field->content; ?></<?php print $field->element_type; ?>>
+  </<?php print $field->inline_html;?>>
+<?php endforeach; ?>
