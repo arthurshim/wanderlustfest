1628774c77791424479443abc7a1100335bf25f4
freeenergy <asborsody@gmail.com>
2014-07-11 19:48:36 +0000

    new social links for taupo arthur

--- sites/all/themes/wanderlust/page-front.tpl.php
+++ sites/all/themes/wanderlust/page-front.tpl.php
@@ -172,7 +172,7 @@ jQuery.noConflict();
       </div>';
        }
    
-      elseif ($subdomain == 'akl' ){
+      elseif ($subdomain == 'akl' ||$subdomain == 'taupo' ){
       print
       '<div class="social-media-links">
         <a class="facebook" href="https://www.facebook.com/WanderlustFestivalNZ?ref=hl" target="_blank" onClick="_gaq.push([\'_trackEvent\', \'Social Media\', \'Facebook\', \'follow\',, false]);">Facebook</a>
